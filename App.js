0fbc2c3cd4bf3732b6e1d5415a032083a36b0e55
Henri Karhatsu <henri@karhatsu.com>
2023-07-12 12:57:51 +0300

    Fix Iltalehti fetching

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
 import ImageTitle from './ImageTitle'
 import NavigationButton from './NavigationButton'
 import ImageScraper from './ImageScraper'
+import ImageListScraper from "./ImageListScraper";
 
 export default class App extends React.Component {
   constructor (props) {
@@ -13,21 +14,18 @@ export default class App extends React.Component {
   }
 
   render() {
-    const ilImageUrlRegex = /<img alt="Fingerpori [0-9.]{10}" src="https:(\/\/assets.ilcdn.fi\/[a-f0-9]+\.gif)"/
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <View style={styles.buttons}>
           <NavigationButton title="Edellinen" left={true} onPress={this.showPrevious}/>
           <NavigationButton title="Seuraava" left={false} onPress={this.showNext} disabled={this.isToday()}/>
         </View>
         <ScrollView>
           <ImageTitle title="Iltalehti"/>
-          <ImageScraper
-            domain="https://www.iltalehti.fi"
+          <ImageListScraper
+            pageUrl="https://www.iltalehti.fi/fingerpori"
+            imageUrlRegex={/img alt="Fingerpori [0-9.]{10}" src="(.*?)" class="list-image"/g}
             index={-this.state.index}
-            imageUrlRegex={ilImageUrlRegex}
-            initialUrl="https://www.iltalehti.fi/fingerpori"
-            previousPagePathRegex={/<a href="(\/fingerpori\/[0-9.]{10})" class="prev">&lt;<\/a>/}
           />
           <ImageTitle title="Helsingin Sanomat"/>
           <ImageScraper
@@ -39,7 +37,7 @@ export default class App extends React.Component {
             previousPagePathRegex={/<a class="article-navlink prev " href="(\/fingerpori\/car-[0-9]+.html)">/}
           />
         </ScrollView>
-      </View>
+      </SafeAreaView>
     );
   }
 
---
+++ ImageListScraper.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, StyleSheet, Text } from "react-native"
+import Comics from "./Comics";
+
+const ImageListScraper = ({ pageUrl, imageUrlRegex, index }) => {
+  const [imageUrls, setImageUrls] = useState([])
+
+  useEffect(() => {
+    fetch(pageUrl).then(response => {
+      response.text().then(text => {
+        const urls = []
+        let match
+        do {
+          match = imageUrlRegex.exec(text)
+          if (match) {
+            urls.push(match[1])
+          }
+        } while (match)
+        setImageUrls(urls)
+      })
+    }).catch(console.error)
+  }, [pageUrl])
+
+  if (imageUrls[index]) {
+    return <Comics imageUrl={imageUrls[index]} />
+  } else if (index !== 0) {
+    return <Text style={s.notFound}>Sarjakuvaa ei löydy</Text>
+  } else {
+    return <ActivityIndicator size="large" color="#a10e1f"/>
+  }
+}
+
+const s = StyleSheet.create({
+  notFound: {
+    marginVertical: 8,
+  },
+})
+
+export default ImageListScraper
